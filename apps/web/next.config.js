b400d8ba3c628854840370bcb1982a68c77afe5e
Philip Rothschild <philiprene89@gmail.com>
2023-08-17 09:13:47 +0200

    Init

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/docs:path*",
+        destination: `${process.env.NEXT_PUBLIC_DOCS_URL}/:path*`,
+      },
+    ];
+  },
 };
