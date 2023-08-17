9a187001abfa353754cea6545d7f10a0bc18b8b7
Philip Rothschild <philiprene89@gmail.com>
2023-08-17 09:20:39 +0200

    ..

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Link href="/web">Web</Link>
+        {children}
+      </body>
     </html>
   );
 }
--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  async rewrites() {
+    return [
+      {
+        source: "/web:path*",
+        destination: `${process.env.NEXT_PUBLIC_WEB_URL}/:path*`,
+      },
+    ];
+  },
 };
